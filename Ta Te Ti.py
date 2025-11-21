a7a8d5a56174a04c85b8d44eebf50235709b92b6
Malcom Foca <106889320+m4lcom@users.noreply.github.com>
2025-11-21 19:43:29 -0300

    Refactor Ta Te Ti game with improved function names
    feat: refactor Tic-Tac-Toe code to English
    
    - Renamed functions and variables to English (PEP8 snake_case)
    - Simplified input validation with isdigit() and divmod
    - Compact winner check using all() for rows, columns, and diagonals
    - Encapsulated game flow in main() for better modularity
    - Updated console messages to English ("You win!", "You lose!", "Draw!")

--- Ta Te Ti.py
+++ Ta Te Ti.py
@@ -1,98 +1,80 @@
-
 from random import randrange
 
-def MostrarTablero(tablero):
-   
-    print("+-------" * 3,"+", sep="")
-    for fila in range(3):
-        print("|       " * 3,"|", sep="")
+def show_board(board):
+    print("+-------" * 3, "+", sep="")
+    for row in range(3):
+        print("|       " * 3, "|", sep="")
         for col in range(3):
-            print("|   " + str(tablero[fila][col]) + "   ", end="")
+            print("|   " + str(board[row][col]) + "   ", end="")
         print("|")
-        print("|       " * 3,"|",sep="")
-        print("+-------" * 3,"+",sep="")
+        print("|       " * 3, "|", sep="")
+    print("+-------" * 3, "+", sep="")
 
-def SiguienteMovimiento(tablero):
-
-    ok = False    
-    while not ok:
-        movimiento = input('Ingresa tu movimiento: ') 
-        ok = len(movimiento) == 1 and movimiento >= '1' and movimiento <= '9' 
-        if not ok:
-            print('Movimiento erroneo, ingresalo nuevamente') 
+def next_move(board):
+    while True:
+        move = input("Enter your move (1-9): ")
+        if not move.isdigit():
+            print("Invalid input, enter a number from 1 to 9.")
             continue
-        movimiento = int(movimiento) - 1     
-        fila = movimiento // 3     
-        col = movimiento % 3        
-        eleccion = tablero[fila][col]    
-        ok = eleccion not in ['O','X'] 
-        if not ok:   
-            print('Cuadro ocupado, ingresalo nuevamente!')
+        move = int(move) - 1
+        if move < 0 or move > 8:
+            print("Number out of range, try again.")
             continue
-    tablero[fila][col] = 'O'    
+        row, col = divmod(move, 3)
+        if board[row][col] in ['O', 'X']:
+            print("Square already taken, try again.")
+            continue
+        board[row][col] = 'O'
+        break
 
-def LugaresVacios(tablero):
+def empty_squares(board):
+    return [(row, col) for row in range(3) for col in range(3) if board[row][col] not in ['O', 'X']]
 
-    libre = []    
-    for fila in range(3): 
-        for col in range(3):
-            if tablero[fila][col] not in ['O','X']: 
-                libre.append((fila,col)) 
-    return libre
-
-def Ganador(tablero, simbolo):
-
-    if simbolo == 'X':    
-        quien = 'yo'    
-    elif simbolo == "O": 
-        quien = 'tu'    
-    else:
-        quien = None    
-    diagonal1 = diagonal2 = True 
-    for k in range(3):
-        if tablero[k][0] == simbolo and tablero[k][1] == simbolo and tablero[k][2] == simbolo:    
-            return quien
-        if tablero[0][k] == simbolo and tablero[1][k] == simbolo and tablero[2][k] == simbolo: 
-            return quien
-        if tablero[k][k] != simbolo: 
-            diagonal1 = False
-        if tablero[2 - k][2 - k] != simbolo: 
-            diagonal2 = False
-    if diagonal1 or diagonal2:
-        return quien
+def winner(board, symbol):
+    # Rows and columns
+    for i in range(3):
+        if all(board[i][j] == symbol for j in range(3)):
+            return symbol
+        if all(board[j][i] == symbol for j in range(3)):
+            return symbol
+    # Diagonals
+    if all(board[i][i] == symbol for i in range(3)):
+        return symbol
+    if all(board[i][2 - i] == symbol for i in range(3)):
+        return symbol
     return None
 
-def MovimientoMaquina(tablero):
+def machine_move(board):
+    free = empty_squares(board)
+    if free:
+        row, col = free[randrange(len(free))]
+        board[row][col] = 'X'
 
-  libres = LugaresVacios(tablero) 
-  cuenta = len(libres)
-  if cuenta > 0: 
-    lugar_maquina = randrange(cuenta)
-    fila, col = libres[lugar_maquina]
-    tablero[fila][col] = 'X'
+def main():
+    board = [[3 * j + i + 1 for i in range(3)] for j in range(3)]
+    board[1][1] = 'X'  # Machine starts in the center
+    human_turn = True
+    game_winner = None
 
+    while empty_squares(board):
+        show_board(board)
+        if human_turn:
+            next_move(board)
+            game_winner = winner(board, 'O')
+        else:
+            machine_move(board)
+            game_winner = winner(board, 'X')
+        if game_winner:
+            break
+        human_turn = not human_turn
 
-tablero = [ [3 * j + i + 1 for i in range(3)] for j in range(3) ] 
-tablero[1][1] = 'X' 
-libre = LugaresVacios(tablero)
-turno_humano = True 
-while len(libre):
-    MostrarTablero(tablero)
-    if turno_humano:
-        SiguienteMovimiento(tablero)
-        ganador = Ganador(tablero,'O')
-    else:    
-        MovimientoMaquina(tablero)
-        ganador = Ganador(tablero,'X')
-    if ganador != None:
-        break
-    turno_humano = not turno_humano        
-    libre = LugaresVacios(tablero)
+    show_board(board)
+    if game_winner == 'O':
+        print("You win!")
+    elif game_winner == 'X':
+        print("You lose!")
+    else:
+        print("Draw!")
 
-MostrarTablero(tablero)
-if ganador == 'tu':
-    print('Ganaste!')
-elif ganador == 'yo':
-    print('Perdiste!')
-else:
-    print('Empate!')
+if __name__ == "__main__":
+    main()
